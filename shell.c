6b8dc2b26145d9aee7b04696d716cf59e959d9a0
Martti <martti@aukia.com>
2019-11-08 20:33:56 +0000

    return shell.c to example version

--- shell.c
+++ shell.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <string.h>
-#include "mymemory.c"
+#include "mymemory.h"
 
 int main() {
   printf("shell> start\n");
 
   initialize();
 
-  char* ptr1 = (char*)mymalloc(1014);
+  char* ptr1 = (char*)mymalloc(10);
   strcpy(ptr1, "this test");
   printf("shell> content of allocated memory: %s\n", ptr1);
 
   char* ptr2 = (char*)mymalloc(10);
-  strcpy(ptr2, "this test1");
+  strcpy(ptr2, "this test");
   printf("shell> content of allocated memory: %s\n", ptr2);
 
+  printmemory();
   printsegmenttable();
 
   myfree(ptr1);
 
   printmemory();
   printsegmenttable();
-
-  printmemory();
 
   myfree(ptr2);
 
